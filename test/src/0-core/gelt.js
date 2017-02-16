a4e3ba234c0b0bef091817d5d8110da537f13e49
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:41 +0100

    aureooms- > @aureooms/ : update js code

--- test/src/0-core/gelt.js
+++ test/src/0-core/gelt.js
@@ -1,6 +1,6 @@
 import test from 'ava' ;
 
-import { iter , repeat } from 'aureooms-js-itertools' ;
+import { iter , repeat } from '@aureooms/js-itertools' ;
 
 import { gelt } from '../../../src' ;
 
